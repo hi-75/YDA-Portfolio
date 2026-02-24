f9e667a5ed58b72c07d7d127747217ebad20951b
ヒロ <hiroshiridirosu@gmail.com>
2026-02-25 04:11:29 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,20 @@ function openModalEnemy() {
 function closeModalEnemy() {
   document.getElementById("modal-enemy").classList.remove("active");
 }
+function openModalTyping() {
+  document.getElementById("modal-typing").classList.add("active");
+}
 
+function closeModalTyping() {
+  document.getElementById("modal-typing").classList.remove("active");
+}
+function openModalPoke() {
+  document.getElementById("modal-poke").classList.add("active");
+}
+
+function closeModalPoke() {
+  document.getElementById("modal-poke").classList.remove("active");
+}
 // scroll animation
 const targets = document.querySelectorAll(".fade-in");
 window.addEventListener("scroll", () => {
